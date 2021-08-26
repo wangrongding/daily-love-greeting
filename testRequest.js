ed96cc958ab90699d5552935f00402de2b66642e
荣顶 <wangrongding@qq.com>
2021-08-26 18:21:50 +0800

    破解BL解析

--- testRequest.js
+++ testRequest.js
@@ -5,19 +5,22 @@ const { axios } = require("./request");
 //发送请求
 async function send() {
     let movieInfo = await axios.get(
-        `https://vip.bljiex.com/api.php?out=jsonp&wd=${encodeURI("扫黑")}`
+        `https://vip.bljiex.com/api.php?out=jsonp&wd=${encodeURI(
+            process.argv[2] || "扫黑"
+        )}`
         // `https://vip.bljiex.com/api.php?out=jsonp&wd=%E6%89%AB%E9%BB%91&cb=jQuery1820530795540517544_1629970804930&_=1629970804954`
     );
-    /* movieInfo.info.forEach((item) => {
-        console.log(item);
-    }); */
-    console.log(Object.prototype.toString.call(movieInfo));
-    console.log(movieInfo.length);
+    console.log(movieInfo);
     let movieList = JSON.parse(movieInfo.substring(1, movieInfo.length - 2));
-    movieList.info.forEach((item) => {
-        item.title = decodeURI(item.title);
-        item.href = `https://vip.bljiex.com/?index${item.id}-${item.flag}-1.htm`;
-    });
-    console.log(movieList);
+    if (movieList.code != "404") {
+        movieList.info.forEach((item) => {
+            item.title = decodeURI(item.title);
+            item.href = `https://vip.bljiex.com/?index${item.id}-${item.flag}-1.htm`;
+        });
+        console.log(movieList);
+        console.log(process.argv);
+    } else {
+        console.log("暂无资源!");
+    }
 }
 send();
