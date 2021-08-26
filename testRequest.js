f1ea1e8b5fc8c103a476654dd8d1de7316e62441
荣顶 <wangrongding@qq.com>
2021-08-26 18:05:40 +0800

    破解BL解析

--- testRequest.js
+++ testRequest.js
@@ -4,33 +4,20 @@ const nodemailer = require("nodemailer");
 const { axios } = require("./request");
 //发送请求
 async function send() {
-    console.log(
-        `想和你一起看世界:第${parseInt(
-            (new Date() - new Date("2020-08-19")) / 1000 / 60 / 60 / 24
-        )}期`
+    let movieInfo = await axios.get(
+        `https://vip.bljiex.com/api.php?out=jsonp&wd=${encodeURI("扫黑")}`
+        // `https://vip.bljiex.com/api.php?out=jsonp&wd=%E6%89%AB%E9%BB%91&cb=jQuery1820530795540517544_1629970804930&_=1629970804954`
     );
-    //获取每日Bing壁纸
-    let bgInfo = await axios.get(
-        "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1"
-    );
-    console.log(`http://cn.bing.com${bgInfo.images[0].url}`);
-    console.log(bgInfo.images[0].copyright);
-    console.log(bgInfo.images[0].copyrightlink);
-    let weather = await axios.get(
-        `http://wthrcdn.etouch.cn/weather_mini?city=${encodeURI("昌平")}`
-    );
-    let weatherInfo = weather.data;
-    console.log(weatherInfo.city);
-    console.log(weatherInfo.forecast[0].low);
-    console.log(weatherInfo.forecast[0].high);
-    console.log(weatherInfo.forecast[0].fengxiang);
-    console.log(weatherInfo.forecast[0].fengli);
-    console.log(weatherInfo.forecast[0].type);
-    console.log(weatherInfo.ganmao);
-    console.log(weatherInfo.wendu);
-    console.log(new Date().toLocaleDateString());
-    console.log(new Date().toLocaleString("default", { weekday: "long" }));
-    let sentence = await axios.get(`https://chp.shadiao.app/api.php`);
-    console.log(sentence);
+    /* movieInfo.info.forEach((item) => {
+        console.log(item);
+    }); */
+    console.log(Object.prototype.toString.call(movieInfo));
+    console.log(movieInfo.length);
+    let movieList = JSON.parse(movieInfo.substring(1, movieInfo.length - 2));
+    movieList.info.forEach((item) => {
+        item.title = decodeURI(item.title);
+        item.href = `https://vip.bljiex.com/?index${item.id}-${item.flag}-1.htm`;
+    });
+    console.log(movieList);
 }
 send();
